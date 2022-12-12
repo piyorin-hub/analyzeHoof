c78ec4d2fe439fc73f646480e80278185c087c5c
piyorin <noripcode@gmail.com>
2022-12-12 16:12:44 +0900

    commit

---
+++ function/fixLegsDirection.m
@@ -0,0 +1,16 @@
+function newPoint = fixLegsDirection(ptCloud, cutAxes, cutWidth)
+% 蹄がある方向が負の場合、正方向になるよう回転する。
+    if nargin < 3
+        cutWidth = 0.5;
+    end
+    originalData= CalculateCircum(ptCloud, cutAxes, cutWidth);
+    x = originalData(:,1);
+    area = originalData(:,3);
+    
+    [~, I] = max(area);
+    rotAxes = 'z';
+    if x(I) < 0
+        newPoint = rotatedPoint(ptCloud, pi, rotAxes);
+    end
+end
+
