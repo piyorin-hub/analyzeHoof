9fa6b3b61b334229ee11d198f8ce9aaee11b81d3
piyorin <noripcode@gmail.com>
2022-12-13 18:04:34 +0900

    renew main.mlapp

--- function/drawPCaxis3D.m
+++ function/drawPCaxis3D.m
@@ -1,7 +1,11 @@
-function [fig, coeff, score] = drawPCaxis3D(ptData)
+function [fig, coeff, score] = drawPCaxis3D(ptData, isShow)
     % This function draws PC Axis on The plot After calculate by PCA
     % @param data => PointCloud.Location (xyzPoints)
     % 
+    if nargin < 2
+        isShow = false;
+    end
+
     [coeff, score] = pca(ptData);
     [h1, h2, h3] = pcaxisVector(coeff);
     meanData = mean(ptData);
@@ -12,19 +16,20 @@ function [fig, coeff, score] = drawPCaxis3D(ptData)
     z = meanData(:,3);
     Z = [z z z];
     
-    fig = figure('Position', [100 500 500 400]); 
-
-    hold on;
-    xLab = [170 0 0];
-    yLab = [0 170 0];
-    zLab = [0 0 170];
-    quiver3(X, Y, Z, h1, h2, h3,'red','AutoScale', "on", 'AutoScaleFactor',165, 'LineWidth',3.5);
-    text(xLab,yLab,zLab, num2str((1:numel(xLab)).'), 'Color','w', 'FontSize',20);
-    quiver3(X, Y, Z, -h1, -h2, -h3,'red', 'AutoScale', "on", 'AutoScaleFactor',140, 'Marker','o','ShowArrowHead','off', 'LineWidth',3.5);
-    grid on;
-    pcshow(ptData, 'AxesVisibility','on');
-    xlabel('X');ylabel('Y');zlabel('Z');
-    hold off;
+    if ~isShow
+        fig = figure('Position', [100 500 500 400]); 
+        hold on;
+        xLab = [170 0 0];
+        yLab = [0 170 0];
+        zLab = [0 0 170];
+        quiver3(X, Y, Z, h1, h2, h3,'red','AutoScale', "on", 'AutoScaleFactor',165, 'LineWidth',3.5);
+        text(xLab,yLab,zLab, num2str((1:numel(xLab)).'), 'Color','w', 'FontSize',20);
+        quiver3(X, Y, Z, -h1, -h2, -h3,'red', 'AutoScale', "on", 'AutoScaleFactor',140, 'Marker','o','ShowArrowHead','off', 'LineWidth',3.5);
+        grid on;
+        pcshow(ptData, 'AxesVisibility','on');
+        xlabel('X');ylabel('Y');zlabel('Z');
+        hold off;
+    end
 end
 
 
